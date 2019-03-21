f76fadf8df424cb5fac210d9a005588dc9d5cba9
WendyRod <44566273+WendyRod@users.noreply.github.com>
2019-03-20 20:19:24 -0600

    Cambios de prueba

--- Easy Enrollment/Easy Enrollment/Login.aspx.cs
+++ Easy Enrollment/Easy Enrollment/Login.aspx.cs
@@ -11,8 +11,7 @@ namespace Easy_Enrollment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Holaaaa
-            //Helou
+            
         }
     }
 }
